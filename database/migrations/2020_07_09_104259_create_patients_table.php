8d7b74fdbd18725f30917c031ad9169a9cdac1e6
osama-eltayar <osamaeltiar@yahoo.com>
2020-07-10 23:21:18 +0200

    add databse structure
    .admin table for admins
    . painTypes table for pains list
    . morph users to patient or doctors
    . patient table for other patients data
    .doctors table for other doctors data
    .appointments table for appointments
    .jobs table for queue

---
+++ database/migrations/2020_07_09_104259_create_patients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('mobile', 13);
+            $table->string('country', 30);
+            $table->enum('gender', ['male', 'female'])->default('male');
+            $table->string('occupation', 100)->nullable();
+            $table->integer('painType_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
---
+++ database/migrations/2020_07_09_164212_add_birth_date_to_patients.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddBirthDateToPatients extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patients', function (Blueprint $table) {
+            $table->date('birth_date')->after('occupation');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('patients', function (Blueprint $table) {
+            //
+        });
+    }
+}
